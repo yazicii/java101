343c191b82f1434964ba5313df16cccc2527c3d8
yazicii <o1yazicii@gmail.com>
2022-04-05 18:41:17 +0300

    VucutKitleIndeksi.java
    Vücut kitle indeks programı

---
+++ VucutKitleIndeksi.java
@@ -0,0 +1,29 @@
+package Java101;
+
+import java.util.Scanner;
+
+//Vücut Kitle İndeksi Hesaplama
+//Java ile kullanıcıdan boy ve kilo değerlerini alıp bir değişkene atayın. Aşağıdaki formüle göre kullanıcının "Vücut Kitle İndeks" değerini hesaplayıp ekrana yazdırın.
+//Formül
+//Kilo (kg) / Boy(m) * Boy(m)
+public class VucutKitleIndeksi {
+    public static void main(String[] args) {
+        double boy, kilo, kitleIndeksi;
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz : ");
+        boy = input.nextDouble();
+        System.out.print("Lütfen kilonuzu giriniz : ");
+        kilo = input.nextDouble();
+
+        kitleIndeksi = kilo / (boy * boy);
+        System.out.println("Vücut Kitle İndeksiniz : "+kitleIndeksi);
+
+
+
+
+
+
+    }
+}
