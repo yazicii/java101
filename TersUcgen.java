6f4658ef2c341e4473d0dcb2725b2ca123b46e1a
yazicii <o1yazicii@gmail.com>
2022-05-20 14:32:44 +0300

    TersUcgen.java

---
+++ TersUcgen.java
@@ -0,0 +1,22 @@
+package Java101;
+import java.util.Scanner;
+
+public class TersUcgen {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Basamaka sayısı: ");
+        int n = scan.nextInt();
+
+        for (int i = 0; i<=n; i++){
+            for (int j = 0; j<=i; j++){
+                System.out.print(" ");
+            }
+            for (int k = (2*n)-1; k >=2*i+1; k--){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+    }
+}
