744856bc1b75d1c7b8f2fb60fc3d7731705a1dfc
oguzhan.yazici <oguzhan.yazici@enuygun.com>
2023-04-15 22:41:52 +0300

    Create Transpoz.java

---
+++ Transpoz.java
@@ -0,0 +1,48 @@
+package Java101;
+
+import java.util.Scanner;
+
+public class Transpoz {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = scanner.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        int columns = scanner.nextInt();
+
+        int[][] matrix = new int[rows][columns];
+
+        System.out.println("Enter matrix elements:");
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+
+        int[][] transpose = new int[columns][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+
+        System.out.println("Original matrix:");
+        printMatrix(matrix);
+
+        System.out.println("Transposed matrix:");
+        printMatrix(transpose);
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
